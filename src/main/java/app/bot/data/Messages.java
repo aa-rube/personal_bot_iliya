3c19e9ea15328e3d44ba642245e8079d459c59ab
alexr <alexrub93@gmail.com>
2025-07-20 21:20:47 -0300

    1 вопрос

--- src/main/java/app/bot/data/Messages.java
+++ src/main/java/app/bot/data/Messages.java
@@ -11,97 +11,124 @@ import java.util.Map;
 public class Messages {
 
     public static Object subscribeMsg(Update update, Long chatId, int msgId, Map<Partner, Boolean> results) {
-
-
         if (msgId == -1) {
-            String s = "Привет, " + UpdateNameExtractor.extractFullName(update) + "! Чтобы попасть в закрытый чат <b>«С GPT-на-Ты: Клуб Доверия к Нейросетям»</b>" +
-                    " с пошаговыми инструкциями по установке ChatGPT, сначала подпишитесь на мой канал  " +
-                    LinkWrapper.wrapTextInLink("Mr.SuperNew", "https://t.me/+DBb3T3wGHd8xMzgy") +
-                    ". После подписки нажмите «Проверить подписку»  и доступ откроется автоматически.";
-            return TelegramData.getSendMessage(chatId, s, Keyboards.subscribe(results));
+            String name = UpdateNameExtractor.extractFullName(update);
+            String text = """
+                    Привет, {name}! Чтобы попасть в закрытый чат <b>«С GPT-на-Ты: Клуб Доверия к Нейросетям»</b>,
+                    с пошаговыми инструкциями по установке ChatGPT, сначала подпишитесь на мой канал 
+                    {link}. После подписки нажмите «Проверить подписку» и доступ откроется автоматически.
+                    """
+                    .replace("{name}", name)
+                    .replace("{link}", LinkWrapper.wrapTextInLink("Mr.SuperNew", "https://t.me/+DBb3T3wGHd8xMzgy"));
+            return TelegramData.getSendMessage(chatId, text, Keyboards.subscribe(results));
         } else {
-            String s = "Похоже, вы ещё не подписаны. Перейдите по ссылке, подпишитесь и попробуйте снова \uD83D\uDE42";
-            return TelegramData.getEditMessage(chatId, s, Keyboards.subscribe(results), msgId);
+            String text = """
+                    Похоже, вы ещё не подписаны. Перейдите по ссылке, подпишитесь и попробуйте снова 🙂
+                    """;
+            return TelegramData.getEditMessage(chatId, text, Keyboards.subscribe(results), msgId);
         }
     }
 
     public static Object mainMenu(Long chatId, int msgId) {
-        String s = "Главное меню";
-        if (msgId < 0) {
-            return TelegramData.getSendMessage(chatId, s, Keyboards.mainKb());
-        } else {
-            return TelegramData.getEditMessage(chatId, s, Keyboards.mainKb(), msgId);
-        }
+        String text = "Главное меню";
+        return msgId < 0
+                ? TelegramData.getSendMessage(chatId, text, Keyboards.mainKb())
+                : TelegramData.getEditMessage(chatId, text, Keyboards.mainKb(), msgId);
     }
 
     public static Object myBolls(Long chatId, int msgId, Map<String, String> userData) {
-        String s = "Баланс: <b>" + userData.getOrDefault("b", "0") + "</b> баллов." +
-                "\n\nУровень: " + userData.getOrDefault("l", "0") +
-                "\nПриглашённых напрямую: " + userData.getOrDefault("l1", "0") +
-                "\nПриглашённых 2-го уровня: " + userData.getOrDefault("l2", "0");
-        return TelegramData.getEditMessage(chatId, s, Keyboards.mainKb(),msgId);
+        String text = """
+                Баланс: <b>{b}</b> баллов.
+
+                Уровень: {l}
+                Приглашённых напрямую: {l1}
+                Приглашённых 2-го уровня: {l2}
+                """
+                .replace("{b}", userData.getOrDefault("b", "0"))
+                .replace("{l}", userData.getOrDefault("l", "0"))
+                .replace("{l1}", userData.getOrDefault("l1", "0"))
+                .replace("{l2}", userData.getOrDefault("l2", "0"));
+        return TelegramData.getEditMessage(chatId, text, Keyboards.mainKb(), msgId);
     }
 
     public static Object share(Long chatId, int msgId) {
-        String s = "Твоя жизнь уже стала проще и эффективнее с нейросетями? \n" +
-                "\n" +
-                "Поделись этим с окружением  и получай бонусы!\n" +
-                "• Отправь друзьям персональную ссылку ниже.\n" +
-                "• За каждого, кто запустит бота, подпишется на канал и нажмёт «Проверить подписку», начислим 10 баллов.\n" +
-                "• За каждого друга твоего друга — ещё 3 балла.\n" +
-                "• Накопи 100 баллов и получи 1-часовую личную консультацию по нейросетям.\n" +
-                "\n" +
-                "Баллы сохраняются, а призовой каталог будет расширяться. Делитесь ссылкой и собирайте их заранее!\uD83D\uDD17 " +
-                String.format("https://t.me/UstanovkaChatGPTbot?start=%d", chatId);
-        return TelegramData.getEditMessage(chatId, s, Keyboards.mainKb(), msgId);
+        String link = "https://t.me/UstanovkaChatGPTbot?start=" + chatId;
+        String text = """
+                Твоя жизнь уже стала проще и эффективнее с нейросетями?
+
+                Поделись этим с окружением и получай бонусы!
+                • Отправь друзьям персональную ссылку ниже.
+                • За каждого, кто запустит бота, подпишется на канал и нажмёт «Проверить подписку», начислим 10 баллов.
+                • За каждого друга твоего друга — ещё 3 балла.
+                • Накопи 100 баллов и получи 1-часовую личную консультацию по нейросетям.
+
+                Баллы сохраняются, а призовой каталог будет расширяться. Делитесь ссылкой и собирайте их заранее! 🔗
+                {link}
+                """.replace("{link}", link);
+        return TelegramData.getEditMessage(chatId, text, Keyboards.mainKb(), msgId);
     }
 
     public static Object spendBolls(Long chatId, int msgId, Map<String, String> m) {
-        String s = """
+        String text = """
                 Доступные награды:
-                • 1-часовая личная консультация — 100 баллов (reward 1)⚡\s
-                
-                Каталог призов будет расширяться. Копите баллы, чтобы первыми получать новые возможности!""";
+                • 1-часовая личная консультация — 100 баллов (reward 1) ⚡
 
+                Каталог призов будет расширяться. Копите баллы, чтобы первыми получать новые возможности!
+                """;
         long b = Long.parseLong(m.getOrDefault("b", "0"));
-        return TelegramData.getEditMessage(chatId, s, b >= 100 ? Keyboards.award() : Keyboards.mainKb(), msgId);
+        return TelegramData.getEditMessage(chatId, text, b >= 100 ? Keyboards.award() : Keyboards.mainKb(), msgId);
     }
 
     public static Object newUser(Long chatId) {
-        String s = "Новый подтверждённый пользователь";
-        return TelegramData.getSendMessage(chatId, s, null);
+        String text = "Новый подтверждённый пользователь";
+        return TelegramData.getSendMessage(chatId, text, null);
     }
 
     public static Object overInviteLimitForAdmin(Long chatId) {
-        String s = "⚠️ " + chatId + " превысил лимит приглашений на сегодня.";
-        return TelegramData.getSendMessage(chatId, s, null);
+        String text = "⚠️ " + chatId + " превысил лимит приглашений на сегодня.";
+        return TelegramData.getSendMessage(chatId, text, null);
     }
 
     public static Object overInviteLimitForUser(Long chatId) {
-        String s = "⚠️ Вы превысили лимит приглашений на сегодня. Баллы временно не начисляются. Если это ошибка — напишите админу.";
-        return TelegramData.getSendMessage(chatId, s, null);
+        String text = """
+                ⚠️ Вы превысили лимит приглашений на сегодня.
+                Баллы временно не начисляются. Если это ошибка — напишите админу.
+                """;
+        return TelegramData.getSendMessage(chatId, text, null);
     }
 
     public static Object requestAward(Long chatId, int msgId) {
-        String s = "✅ Заявка на «1-часовую консультацию» принята! Мы свяжемся с вами, чтобы выбрать удобное время.";
-        return TelegramData.getEditMessage(chatId, s, null, msgId);
+        String text = """
+                ✅ Заявка на «1-часовую консультацию» принята!
+                Мы свяжемся с вами, чтобы выбрать удобное время.
+                """;
+        return TelegramData.getEditMessage(chatId, text, null, msgId);
     }
 
     public static Object uniqueLink(Long chatId) {
-        String s = "Отлично! 🎉Вот ваш пропуск в " +
-                LinkWrapper.wrapTextInLink("закрытый клуб", "https://t.me/+R_7xy_8KZ244Y2Qx") +
-                "Внутри ждут подробные инструкции и поддержка сообщества. Если возникнут вопросы - задавайте их в группе 'Общий чат'.";
-        return TelegramData.getSendMessage(chatId, s, Keyboards.mainKb());
+        String text = """
+                Отлично! 🎉 Вот ваш пропуск в {link}
+                Внутри ждут подробные инструкции и поддержка сообщества.
+                Если возникнут вопросы — задавайте их в группе «Общий чат».
+                """
+                .replace("{link}", LinkWrapper.wrapTextInLink("закрытый клуб", "https://t.me/+R_7xy_8KZ244Y2Qx"));
+        return TelegramData.getSendMessage(chatId, text, Keyboards.mainKb());
     }
 
     public static Object adminNotificationAward(Long chatId, Long userId, int msgId) {
-        String s = "✅ Получена заявка на «1-часовую консультацию»\n\nПользователь: " + userId + "\n\n";
-        return TelegramData.getEditMessage(chatId, s, null, msgId);
+        String text = """
+                ✅ Получена заявка на «1-часовую консультацию»
+
+                Пользователь: {uid}
+                """.replace("{uid}", String.valueOf(userId));
+        return TelegramData.getEditMessage(chatId, text, null, msgId);
     }
 
-
     public static Object cheers(Long ref, Map<String, String> m) {
-        String s = "\uD83C\uDF89 Поздравляем! Вы достигли уровня {level_emoji} {level_name}. Продолжайте делиться — следующий ранг уже близко.";
-        return TelegramData.getSendMessage(ref, s, null);
+        String text = """
+                🎉 Поздравляем! Вы достигли уровня {level_name}.
+                Продолжайте делиться — следующий ранг уже близко.
+                """.replace("{level_name}", m.getOrDefault("l", ""));
+        return TelegramData.getSendMessage(ref, text, null);
     }
 }
