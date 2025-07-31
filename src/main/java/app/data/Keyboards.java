f7b906e18f3558ff03552f60e5e24045a4c2822c
alexr <alexrub93@gmail.com>
2025-07-31 14:01:30 -0300

    welcomeMessageService + remove msg - edit welcome text message

--- src/main/java/app/data/Keyboards.java
+++ src/main/java/app/data/Keyboards.java
@@ -106,13 +106,15 @@ public class Keyboards {
                         "\uD83C\uDF81 Мои баллы",
                         "\uD83D\uDC65 Пригласить друзей",
                         "\uD83D\uDECD Потратить баллы",
-                        "\uD83D\uDCC5 Бесплатная {b}/100".replace("{b}", String.valueOf(b))
+                        "\uD83D\uDCC5 Бесплатная {b}/100".replace("{b}", String.valueOf(b)),
+                        "\uD83D\uDCAC Платная консультация"
                 },
                 new String[]{
                         "my_bolls_",
                         "share_",
                         "spend_bolls_",
-                        "award_" + (b < 100 ? "no_" : "yes_")
+                        "award_" + (b < 100 ? "no_" : "yes_"),
+                        "https://t.me/MoneyBaires"
                 }
         );
     }
