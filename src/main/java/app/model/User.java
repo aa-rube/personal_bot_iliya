edb56881e8f45a7693e6b52f9e454b481cb67f72
alexr <alexrub93@gmail.com>
2025-08-09 00:36:20 -0300

    check activity

--- src/main/java/app/model/User.java
+++ src/main/java/app/model/User.java
@@ -45,6 +45,8 @@ public class User {
         this.firstSeen = LocalDate.now();
         this.lastAction = LocalDate.now();
 
+        this.warnedAt = 0L;
+
         this.lastSubscribeChecked = System.currentTimeMillis();
         this.isActive = true;
         this.isKickUserFromChat = false;
